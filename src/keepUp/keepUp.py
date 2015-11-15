58e088d1e8c635e093c49fccc52d4a98e2b71146
ma11 <matt70@free.fr>
2015-11-15 23:25:04 +0100

    Filename in code + Authors Add authors in AUTHORS file Filename is now a member of class App. Change the calls to filename to fit with that

--- src/keepUp/keepUp.py
+++ src/keepUp/keepUp.py
@@ -1,14 +1,10 @@
 # -*- coding: UTF-8 -*-
 
-#################################################################
-## FILENAME   : keepUp
-## AUTHOR     : me
-## DATE       : Sun Nov 15 11:49:04 2015
-## COPYRIGHT  : 
-## PROJECT    : keepUp
-## DESCRIPTION: Store data that represent stuff to keep updated 
-##              with handy functions to get older ones, ...
-#################################################################
+# @package KeepUp
+# @author ma11
+# @brief Application to help keeping stuff up to date
+# @details It provides program which stores tags in a file with timestamp
+#   The program can update the timestamps to current time, print every tags and timestamp, or print only tags which timestamp are older than a given period of time.
 
 from optparse import OptionParser
 from time import time,ctime
@@ -20,24 +16,38 @@ from shutil import move
 from os import remove, close
 
 
+# @class App
+# @brief Hold application methods
 class App():
+    # @brief The filename which contains the tags and timestamps
+    filename = 'tags.txt'
+
     def __init__(self,*args,**kwargs):
-        self.filename = "toto"
         for key in kwargs:
             setattr(self,key,kwargs[key])
 
-    def replace(self,file_path, pattern, subst):
+    def getLinesFromFile(self):
+        try:
+            with open(self.filename,'r') as f:
+                lines=f.readlines()
+                f.close()
+        except IOError:
+            print "Error: can't open file "+self.filename+". If the file does not existing start by adding an item first."
+            lines=()
+        return lines
+
+    def replace(self, pattern, subst):
         #Create temp file
         fh, abs_path = mkstemp()
         with open(abs_path,'w') as new_file:
-            with open(file_path) as old_file:
+            with open(self.filename) as old_file:
                 for line in old_file:
                     new_file.write(line.replace(pattern, subst))
         close(fh)
         #Remove original file
-        remove(file_path)
+        remove(self.filename)
         #Move new file
-        move(abs_path, file_path)
+        move(abs_path, self.filename)
 
     def convertToTs(self,duration):
         dur=duration
@@ -48,42 +58,34 @@ class App():
         return eval(dur)
              
 
-    def printList(self,filename):
-        with open(filename,'r') as f:
-            lines = f.readlines()
-            f.close()
-
+    def printList(self):
+        lines=self.getLinesFromFile()
         for li in lines:
             elem = li.split(',')
             print elem[0] +'   --   '+ctime(float(elem[1]))
 
-    def newItem(self,filename,name):
-        with open(filename,'a') as fname:
+    def newItem(self,name):
+        with open(self.filename,'a') as fname:
             fname.write(name+','+str(time())+','+'\n')
             fname.close()
 
-    def extractOlderThan(self,duration,filename):
+    def extractOlderThan(self,duration):
         ts = self.convertToTs(duration)
         limit = time() - ts
-        with open(filename,'r') as f:
-            lines = f.readlines()
-            f.close()
+        lines=self.getLinesFromFile()
         for li in lines:
             elem = li.split(',')
             if float(elem[1])<limit:
                 print elem[0]+'   --   '+ctime(float(elem[1])) 
 
-    def updateItem(self,items,filename):
-        with open(filename,'r') as f:
-            lines = f.readlines()
-            f.close()
-
+    def updateItem(self,items):
+        lines=self.getLinesFromFile()
         itemList = items.split(',')
         for it in itemList:
             for li in lines:
                 elem = li.split(',')
                 if elem[0]==it:
-                    self.replace(filename,elem[0]+','+elem[1],elem[0]+','+str(time()))
+                    self.replace(elem[0]+','+elem[1],elem[0]+','+str(time()))
                     break
 
     def main(self):
@@ -96,19 +98,19 @@ class App():
             optparser.add_option("-o","--older-than",help="Extract items older than DURATION",metavar="DURATION",default="",dest='duration')
 
 	    (options, args) = optparser.parse_args()
+            self.filename = options.filename
 
             if options.printList == True:
-                self.printList(options.filename)
+                self.printList()
 
             if not options.newItem == '':
-                print 'newItem'
-                self.newItem(options.filename,options.newItem)
+                self.newItem(options.newItem)
 
             if not options.duration == '':
-                self.extractOlderThan(options.duration,options.filename)
+                self.extractOlderThan(options.duration)
 
             if not options.updateItem == '':
-                self.updateItem(options.updateItem,options.filename)
+                self.updateItem(options.updateItem)
 
 if __name__ == '__main__':
     app=App(version='alpha',filename='tutu')
