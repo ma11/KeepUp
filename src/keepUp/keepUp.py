5ca436a41b0eb881db386aebb00a36e5aebb17d8
ma11 <matt70@free.fr>
2015-11-16 15:26:43 +0100

    Document and Item class
    The code has been fully documented (issue #2)
    The item are now represented using a class: Item
    Some code is in function to avoid duplicate code (getItemFromFile)

--- src/keepUp/keepUp.py
+++ src/keepUp/keepUp.py
@@ -15,6 +15,34 @@ from tempfile import mkstemp
 from shutil import move
 from os import remove, close
 
+def convertToTs(duration):
+    dur=duration
+    mapping={'s':'*1+','m':'*60+','h':'*3600+','d':'*86400+','w':'*604800+','M':'*2592000+','Y':'*31536000+'}
+    for k,v in mapping.iteritems():
+        dur=dur.replace(k,v)
+    dur=dur[:-1]
+    return eval(dur)
+             
+# @class Item
+# @brief Represent an item to keep udated
+# @detail An item hold a tag and a timestamp.
+# It has methods to test wether the timestamp is older than a given duration
+class Item():
+    # @brief The tag name
+    tag = ''
+
+    # @biref The timestamp of the tag
+    timestamp = 0.0 
+
+    def __init__(self,tag,ts=time()):
+        self.tag = tag
+        self.timestamp = float(ts)
+
+    ## Return true if item is older than the given duration (ie. it it is older than 1 week)
+    # @param duration A string representing the duration. For example, a duration of one day two hours three minutes would be '1d2h3m'
+    def isOlderThan(self,duration):
+        return self.timestamp < (time() - convertToTs(duration))
+
 
 # @class App
 # @brief Hold application methods
@@ -26,16 +54,20 @@ class App():
         for key in kwargs:
             setattr(self,key,kwargs[key])
 
+    ## Read the file self.filename and extract lines
     def getLinesFromFile(self):
         try:
             with open(self.filename,'r') as f:
                 lines=f.readlines()
-                f.close()
         except IOError:
             print "Error: can't open file "+self.filename+". If the file does not existing start by adding an item first."
             lines=()
         return lines
 
+    ## A usefull function to replace a pattern in file self.filename (see http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python)
+    # @detail It create a new temporary file, copy and replace line after line, and move the temporary file in the place of self.filename
+    # @param pattern The string to replace
+    # @param subst The string pattern is to be replaced by
     def replace(self, pattern, subst):
         #Create temp file
         fh, abs_path = mkstemp()
@@ -49,35 +81,34 @@ class App():
         #Move new file
         move(abs_path, self.filename)
 
-    def convertToTs(self,duration):
-        dur=duration
-        mapping={'s':'*1+','m':'*60+','h':'*3600+','d':'*86400+','w':'*604800+','M':'*2592000+','Y':'*31536000+'}
-        for k,v in mapping.iteritems():
-            dur=dur.replace(k,v)
-        dur=dur[:-1]
-        return eval(dur)
-             
-
-    def printList(self):
+    ## Return a list of Item() elements stored in self.filename
+    def getItemsFromFile(self):
         lines=self.getLinesFromFile()
-        for li in lines:
-            elem = li.split(',')
-            print elem[0] +'   --   '+ctime(float(elem[1]))
+        return [  Item(li.split(',')[0],li.split(',')[1]) for li in lines]
 
+    ## Print the list of items contained in file self.filename
+    def printList(self):
+        its = self.getItemsFromFile()
+        for it in its:
+            print '{0:10} | {1:10}'.format(it.tag, ctime(it.timestamp))
+
+    ## Create a new Item() element with current timestamp.
+    # @param name New item's name
     def newItem(self,name):
         with open(self.filename,'a') as fname:
             fname.write(name+','+str(time())+','+'\n')
-            fname.close()
 
+    ## Print only items older than a given duration
+    # @param duration The duration items are supposed to be older than. For instance, duration older than one month and two days is written '1M2d'
     def extractOlderThan(self,duration):
-        ts = self.convertToTs(duration)
-        limit = time() - ts
-        lines=self.getLinesFromFile()
-        for li in lines:
-            elem = li.split(',')
-            if float(elem[1])<limit:
-                print elem[0]+'   --   '+ctime(float(elem[1])) 
+        its = self.getItemsFromFile()
+        ito = [ it for it in its if it.isOlderThan(duration) ]
 
+        for it in ito:
+            print '{0:10} | {1:10}'.format(it.tag, ctime(it.timestamp))
+
+    ## Update timestamp of an item
+    # @param item Tag of the item to be updated
     def updateItem(self,items):
         lines=self.getLinesFromFile()
         itemList = items.split(',')
@@ -88,6 +119,7 @@ class App():
                     self.replace(elem[0]+','+elem[1],elem[0]+','+str(time()))
                     break
 
+    ## Runtime of the application
     def main(self):
 	    optparser = OptionParser(version='%prog '+str(self.version))
 
